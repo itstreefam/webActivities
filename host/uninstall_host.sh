3b5f8f2352785bf460932cccb154ce05b7a90575
TriPham2110 <thientri399@gmail.com>
2022-06-07 12:10:39 -0500

    Added macOS support

---
+++ host/uninstall_host.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/mv2-archive/api/nativeMessaging/host/uninstall_host.sh
+set -e
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
+    TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+  else
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  fi
+else
+  if [ "$(whoami)" = "root" ]; then
+    TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+  else
+    TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+  fi
+fi
+
+HOST_NAME=savedat-mac
+rm "$TARGET_DIR/savedat-mac.json"
+echo "Native messaging host $HOST_NAME has been uninstalled."
