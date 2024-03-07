542dc823f1d16939b3ce8567fef36b0fd5b77dc0
Tri Pham <p.tri@wustl.edu>
2024-03-07 02:55:03 -0600

    Experiencing mouse and the draggable element drift apart during dragging

--- src/draggablerecording.js
+++ src/draggablerecording.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Draggable from 'react-draggable';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Draggable>
-                <div style={{ padding: '10px', background: 'green', cursor: 'pointer', position: 'fixed', top: '50px', left: '50px' }}>
-                    <p style={{ color: 'gray' }}>Recording</p>
-                </div>
-            </Draggable>
-        );
-    }
-}
+const draggableStyle = {
+    position: 'fixed',
+    bottom: '10px',
+    right: '10px',
+    zIndex: 2147483647,
+    backgroundColor: 'green',
+    color: 'white',
+    padding: '10px',
+    borderRadius: '8px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+};
+
+// Create a functional component for recording bar
+const DraggableRecordingBar = () => {
+    return (
+        <Draggable>
+            <div style={draggableStyle}>
+                <div>🔴 You are screen sharing</div>
+                <button >
+                    Stop Share
+                </button>
+            </div>
+      </Draggable>
+    );
+};
 
 window.onload = function() {
     console.log('Hello from DraggableRecording.js');
@@ -22,12 +36,11 @@ window.onload = function() {
             console.log('testDraggable message received');
             const mountNode = document.createElement('div');
             mountNode.id = 'draggable-recording';
-            mountNode.style.zIndex = '9999';
             document.body.appendChild(mountNode);
 
             // Use createRoot to render the component
-            const root = createRoot(mountNode); // Create a root for the component.
-            root.render(<App />); // Render the component.
+            const root = createRoot(mountNode);
+            root.render(<DraggableRecordingBar />);
         }
     });
 };
